d50df9b176afef2397417bf5c5091ec7f7c20bef
DeepakRavi <94233440+Dkravi93@users.noreply.github.com>
2022-06-09 16:18:09 +0530

    cors added

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
